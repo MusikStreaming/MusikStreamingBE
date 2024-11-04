3acefd005d73479ef05bdc2acef56fd6f1c182b7
zeann3th <longhienly112@gmail.com>
2024-11-04 10:59:15 +0700

    [UPDATE]: Fixing bugs

--- src/middlewares/rate-limit.config.ts
+++ src/middlewares/rate-limit.config.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 const uploadRateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 10,
+  max: 30,
   keyGenerator: (req) => req.params.id,
   handler: (req, res) => {
     res.status(429).json({

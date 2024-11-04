21456c8d8a09bbc9c584c3b40dd3d7e42e8c1ff6
zeann3th <longhienly112@gmail.com>
2024-11-04 11:49:26 +0700

    [UPDATE]: Fixing bugs

--- src/middlewares/rate-limit.config.ts
+++ src/middlewares/rate-limit.config.ts
@@ -12,4 +12,24 @@ const uploadRateLimiter = rateLimit({
   },
 });
 
-export { uploadRateLimiter };
+const authRateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  keyGenerator: (req) => {
+    const ipAddress =
+      req.headers["cf-connecting-ip"] ||
+      req.headers["x-forwarded-for"] ||
+      req.socket.remoteAddress ||
+      "Unknown IP";
+    const userAgent = req.headers["user-agent"] || "Unknown User Agent";
+    return `${ipAddress}::${userAgent}`;
+  },
+  handler: (req, res) => {
+    res.status(429).json({
+      message: "Too many requests, please try again later.",
+      retryAfter: 900,
+    });
+  },
+});
+
+export { uploadRateLimiter, authRateLimiter };

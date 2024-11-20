775523c0853aefbd8fd76edec6b8b79016bd2eaf
zeann3th <longhienly112@gmail.com>
2024-11-20 22:00:24 +0700

    [UPDATE]: Fixing cache request limited by wrapping try catch

--- src/services/redis.ts
+++ src/services/redis.ts
@@ -1,9 +1,40 @@
 import env from "@/env";
-import { Redis } from "@upstash/redis";
+import { Redis, SetCommandOptions } from "@upstash/redis";
 
-const redis = new Redis({
-  url: env.REDIS_URL,
-  token: env.REDIS_TOKEN,
-});
+class RedisUpstash {
+  private readonly client: Redis;
 
+  constructor() {
+    this.client = new Redis({
+      url: env.REDIS_URL,
+      token: env.REDIS_TOKEN,
+    });
+  }
+
+  public async set<TData>(
+    key: string,
+    data: TData,
+    options?: SetCommandOptions,
+  ): Promise<"OK" | TData | null> {
+    try {
+      const res = await this.client.set(key, data, options);
+      return res;
+    } catch (err) {
+      console.log("Cache request limit reached...");
+      return null;
+    }
+  }
+
+  public async get<TData>(key: string): Promise<TData | null> {
+    try {
+      const res = await this.client.get(key);
+      return res as TData | null;
+    } catch (err) {
+      console.log("Cache request limit reached...");
+      return null;
+    }
+  }
+}
+
+const redis = new RedisUpstash();
 export default redis;

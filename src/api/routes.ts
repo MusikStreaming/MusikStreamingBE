144bed08b8b32769cc2239d488719e7597933cd5
zeann3th <longhienly112@gmail.com>
2024-12-07 09:55:06 +0700

    [UPDATE]: Reverting to v1, cache mp3 is an anti pattern :(

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -8,7 +8,6 @@ import { collectionRoutes } from "./collections/routes";
 import { artistRoutes } from "./artists/routes";
 import { searchRoutes } from "./search/routes";
 import { paymentRoutes } from "./payments/routes";
-import { SongController } from "./songs/controller";
 
 const router = Router();
 
@@ -24,11 +23,6 @@ router.use("/v1/auth", IPRateLimiter, authRoutes);
 router.use("/v1/user", userMiddleware, userRoutes);
 
 router.use("/v1/song", songRoutes);
-
-router.use(
-  "/v2/song/:id/presigned/stream",
-  SongController.generatePresignedDownloadURLv2,
-);
 
 router.use("/v1/collection", userMiddleware, collectionRoutes);
 
